a54bad1a81bba5df0c7c2070bed898d2ec23b221
Your Name <you@example.com>
2019-06-29 19:23:24 +0800

    send flie

---
+++ 外星人项目/body/ALIENWARE中国官方网站_files/box.php
@@ -0,0 +1,15 @@
+
+document.write('<style>');
+document.write('.boxqr{background:url(../public/images/618-1.jpg) no-repeat center 0;height:452px;display:none}');
+document.write('.box{background:url(../public/images/box_exclusive.jpg) no-repeat center 0;height:220px;}');
+document.write('.box.exclusive{background:url(../public/images/box_exclusive.jpg) no-repeat center 0;}');
+document.write('.box .wrap{position:relative;height:220px;}');
+document.write('.box .wrap a{position:absolute;left:0;top:59px;}');
+document.write('</style>');
+document.write('<div class="boxqr week-6"></div>');
+document.write('<div class="box " id="BOX-BD">');
+document.write('<div class="wrap">');
+document.write('<a href="http://q.url.cn/cdFei3?_type=wpa&qidian=true" target="_blank"><img src="../public/images/spacer.gif?image" width="1200" height="118" /></a>');
+document.write('<a href="http://q.url.cn/cdFei3?_type=wpa&qidian=true" target="_blank" style="left:1077px;top:177px;"><img src="../public/images/spacer.gif?link" width="123" height="28" /></a>');
+document.write('</div>');
+document.write('</div>');
---
+++ 外星人项目/body/ALIENWARE中国官方网站_files/serverComponent.php
@@ -0,0 +1,2 @@
+
+Bootstrapper._serverTime = '2019-06-29 02:30:27'; Bootstrapper._clientIP = '0.0.0.0'; Bootstrapper.insertPageFiles(['https://nexus.ensighten.com/dell/external/code/c52f07e4cc9c7261648376337948131c.js?conditionId0=421951','https://nexus.ensighten.com/dell/external/code/3482034cc9b7fc91a110c82c579b3e81.js?conditionId0=371316']);Bootstrapper.setPageSpecificDataDefinitionIds([10064,58705,56946,58310,58407,58700]);
